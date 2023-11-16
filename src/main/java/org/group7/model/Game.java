9af5580061e927bdfdf5c5e1b1c80bd6f708fc5b
Kevin Collins <nivek049@gmail.com>
2023-11-16 13:56:30 +0100

    Added a new game button and refactored code in GameWindow as well as Game (worked on by the group together)

--- src/main/java/org/group7/model/Game.java
+++ src/main/java/org/group7/model/Game.java
@@ -1,18 +1,29 @@
 package org.group7.model;
+import java.util.List;
 
 public class Game {
 
     private Dice dice;
     private Board board;
     private Player[] players;
+    private Player currentPlayer;
 
     public Game() {
         this.dice = Dice.getInstance();
         this.board = new Board();
         this.players = new Player[4];
+
+        int i = 0;
+        this.currentPlayer = players[i];
+        int diceRoll = rollDice();
+        List<Piece> currentPieces = players[i].getPieces();
+        Piece movingPiece = players[i].choosePiece();
+        //movingPiece.
     }
 
     public int rollDice() {
         return dice.roll();
     }
 }
+
+
