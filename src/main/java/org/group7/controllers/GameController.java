a84009850e59b5f6d5a47d3215691a4be96fd40b
Lee4-M <liamn.mayor@gmail.com>
2023-12-01 02:16:52 +0100

    Added dice roll listener

--- src/main/java/org/group7/controllers/GameController.java
+++ src/main/java/org/group7/controllers/GameController.java
@@ -2,60 +2,36 @@ package org.group7.controllers;
 
 import org.group7.model.Game;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.nio.Buffer;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class GameController{
     private JButton rollDiceButton = new JButton();
     private JButton newGameButton = new JButton("New game");
     private List<JButton> buttons = new ArrayList<>();
-    private List<Point> imageCoordinates = new ArrayList<>();
+    private RollDiceListener rollDiceListener;
     private Game game;
 
     public GameController(Game game){
         this.game = game;
-        initDiceImageCoordinates();
-        addListeners();
+        this.rollDiceListener = new RollDiceListener(rollDiceButton, game);
         addButtonsToListOfButtons();
+        addListeners();
     }
 
     public void addListeners(){
-        rollDiceButton.addActionListener(e -> rollDice());
+        Timer timer = new Timer(80, rollDiceListener);
+        rollDiceButton.addActionListener(e -> {
+            rollDice(timer);
+        });
     }
 
-    private void initDiceImageCoordinates(){
-        Collections.addAll(imageCoordinates, new Point(0, 0), new Point(205, 0),
-                new Point(408, 0), new Point(0, 203), new Point(205, 203),
-                new Point(408, 203));
+    private void rollDice(Timer timer){
+        rollDiceButton.setEnabled(false);
+        timer.start();
     }
 
-    private void rollDice(){
-        int diceRollResult = game.rollDice() - 1;
-        System.out.println(diceRollResult);
-        BufferedImage image = new BufferedImage(205, 205, BufferedImage.TYPE_INT_RGB);
-        try{
-            image = ImageIO.read(new File("src/main/resources/dices2.png"));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        Icon icon = new ImageIcon(image.getSubimage(
-                (int) imageCoordinates.get(diceRollResult).getX(),
-                (int) imageCoordinates.get(diceRollResult).getY(),
-                205, 205));
-        rollDiceButton.setIcon(icon);
-    }
     public void addButtonsToListOfButtons(){
         buttons.add(this.rollDiceButton);
         buttons.add(this.newGameButton);
