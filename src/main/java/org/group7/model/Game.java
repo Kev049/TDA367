40d4d18e7620581e5819716f84f4a919ec2c2430
Kevin Collins <nivek049@gmail.com>
2023-12-08 13:52:09 +0100

    Resolving merge conflicts

--- src/main/java/org/group7/model/Game.java
+++ src/main/java/org/group7/model/Game.java
@@ -18,7 +18,6 @@ public class Game implements StringObservable, Observer {
     private Player currentPlayer;
     private int currentPlayerNumber;
     private int turnNumber;
-    private int amountOfPlayers; //TODO:Ändra så att mängden players ställs in på ett annat ställe
     private final int turnNumberStart = 0;
 
     private GameState gameState;
@@ -48,12 +47,6 @@ public class Game implements StringObservable, Observer {
         }
         this.currentPlayerNumber = 0;
         this.currentPlayer = players[currentPlayerNumber];
-    }
-
-    private void initPlayers(){
-        for( int i = 0; i < this.amountOfPlayers - 1; i++){
-            this.players[i] = PlayerFactory.createPlayer(this.colorArray[i]);
-        }
     }
 
     public int rollDice() {         //TODO implementera så att en state bestämmer vad som händer. RollState - rulla tärning, MoveState - gör inget (man ska flytta pjäs)
