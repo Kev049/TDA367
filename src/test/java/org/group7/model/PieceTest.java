d0608cb7af176e6a2cddcc840d23d30ec7857614
Simon Hammerlid <simon.hammerlid@gmail.com>
2023-12-14 18:09:20 +0100

    fixade piecetests

--- src/test/java/org/group7/model/PieceTest.java
+++ src/test/java/org/group7/model/PieceTest.java
@@ -1,8 +1,8 @@
 package org.group7.model;
 
 import org.group7.model.board.Board;
-import org.group7.model.board.Board.Entities.Piece;
 import org.group7.model.board.IMoveHandler;
+import org.group7.model.board.entities.piece.Piece;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class PieceTest {
 
