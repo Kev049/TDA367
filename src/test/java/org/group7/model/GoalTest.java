2f02fbfd1c3efa11d51749d09416db1521b92556
Simon Hammerlid <simon.hammerlid@gmail.com>
2023-12-14 18:15:44 +0100

    fixade imports

--- src/test/java/org/group7/model/GoalTest.java
+++ src/test/java/org/group7/model/GoalTest.java
@@ -1,13 +1,16 @@
 package org.group7.model;
 
 import org.group7.model.board.Board;
-import org.group7.model.board.Board.Entities.Piece;
+import org.group7.model.board.Goal;
 import org.group7.model.board.IMoveHandler;
+import org.group7.model.board.entities.piece.Piece;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GoalTest {
 
